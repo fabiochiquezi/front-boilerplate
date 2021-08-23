d35882e20cee0f11ca174728937b58712cee0fbb
Fábio Chiquezi <fabiochiquezi@gmail.com>
2021-08-23 12:21:15 -0300

    v1 done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import type { NextPage } from 'next'
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+    font-size: 32px;
+    color: red;
+`
 
 const Home: NextPage = () => {
   return (
     <div className="container">
-        <h1>Hello World!!!</h1>
+        <H1>Hello World!!!</H1>
     </div>
   )
 }
